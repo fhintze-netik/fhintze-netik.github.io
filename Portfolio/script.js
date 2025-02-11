7755cfd6c1989c2969b251cc0250e614ebb09508
Felix Hintze <fhintze@netik.de>
2025-02-11 13:46:13 +0100

    Portfolio Refactoring

---
+++ Portfolio/script.js
@@ -0,0 +1,18 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbz6eVl5-MoaHz3IN6qkzWIpS-QHshI_UowOmS41utu0Vl3tbrBC0RfIaaMtSopRhVRs/exec";
+const form = document.forms["contact-us"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
